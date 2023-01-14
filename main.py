7290a876cc51c3efa57b87fdeda4f48e4bc91859
Alex Damian <alexgoth1992@hotmail.com>
2023-01-14 15:04:16 -0500

    laboratorio}

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-nombre = input("ingrese su nombre")
-apellido = input("ingrese su nombre")
-edad = input("ingrese su edad")
-fullname = "mi nombre es" + nombre + "mi apellido es" + apellido + "y tengo una edad de" + edad
-print(fullname)
+#nombre = input("ingrese su nombre")
+#apellido = input("ingrese su nombre")
+#edad = input("ingrese su edad")
+#fullname = "mi nombre es" + nombre + "mi apellido es" + apellido + "y tengo una edad de" + edad
+#print(fullname)
+
+
+
+print ("*****")
+name1= "Alex"
+text= "hola como estas "
+nombre= input ("ingrese su nombre ")
+#print(name1.upper())
+#print(name1 == nombre)
+#print(len(name1))
+#fulltext = text + nombre
+#fulltextv1 = f"{text} {nombre} ..!"
+#print(fulltext)
+#print(fulltextv1)
+
+print(nombre == nombre[::-1])
+
+
+
+
+
 
