3990c5831ef555ba68a3d5161921b7a0a5d4df5d
alexyugcha1992 <116452593+alexyugcha1992@users.noreply.github.com>
2023-01-28 15:28:01 -0500

    Examen

---
+++ Code/Examen.py
@@ -0,0 +1,13 @@
+#Ingresar por consola los valores de 6 digitos
+
+v1= int(input ("ingrese primer valor"))
+v2= int(input ("ingrese segundo valor"))
+v3= int(input ("ingrese tercer valor"))
+v4= int(input ("ingrese cuarto valor"))
+v5= int(input ("ingrese quinto valor"))
+v6= int(input ("ingrese sexto valor"))
+print("valor total", v1+v2+v3+v4+v5+v6)
+t = ((v1+v2+v3+v4+v5+v6) * 1.12)
+print("el valor de la compra es:", t)
+
+#calcular la suma de los valores y su promedio
